82fb54126e6537a6335958fd618abaf225e06616
Mohamed3li454 <mohhamed4413@gmail.com>
2023-10-01 09:21:53 +0300

    add a searching button

--- lib/Animations/particles_animation.dart
+++ lib/Animations/particles_animation.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:animated_button/animated_button.dart';
+// ignore: unnecessary_import
+import 'package:flutter/cupertino.dart';
+import 'package:wither_app/views/searching_view.dart';
 
 // ignore: camel_case_types
 class particle_animation extends StatefulWidget {
@@ -25,33 +29,65 @@ class _particle_animationState extends State<particle_animation> {
           ),
         ),
       ),
-      // backgroundColor: const Color(0xff437BFF),
+      // backgroundColor: Color.fromARGB(255, 78, 72, 58),
       // ignore: avoid_unnecessary_containers
-      body: Center(
-        child: AnimatedTextKit(
-          animatedTexts: [
-            TypewriterAnimatedText(
-              "There's no Action 😔",
-              speed: const Duration(milliseconds: 90),
-              textStyle: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const SizedBox(
+            height: 200,
+          ),
+          Center(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  "There's no Action 😔",
+                  speed: const Duration(milliseconds: 90),
+                  textStyle: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TypewriterAnimatedText(
+                  'Start searching now 🔍',
+                  speed: const Duration(milliseconds: 70),
+                  textStyle: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+              isRepeatingAnimation: true,
+              repeatForever: true,
+              displayFullTextOnTap: true,
+              stopPauseOnTap: false,
             ),
-            TypewriterAnimatedText(
-              'Start searching now 🔍',
-              speed: const Duration(milliseconds: 70),
-              textStyle: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          ),
+          const SizedBox(height: 200),
+          Center(
+            child: AnimatedButton(
+              // ignore: sort_child_properties_last
+              child: const Text(
+                'Searching 🔍',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            )
-          ],
-          isRepeatingAnimation: true,
-          repeatForever: true,
-          displayFullTextOnTap: true,
-          stopPauseOnTap: false,
-        ),
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) {
+                    return const searching_view();
+                  },
+                ));
+              },
+              enabled: true,
+              shadowDegree: ShadowDegree.light,
+            ),
+          ),
+        ],
       ),
     );
   }
