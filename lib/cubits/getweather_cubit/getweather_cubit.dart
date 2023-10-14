71f2d72d4e965b1a00a7e0ba50566580aa12989e
Mohamed3li454 <mohhamed4413@gmail.com>
2023-10-14 16:44:45 +0300

    update

--- lib/cubits/getweather_cubit/getweather_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-// ignore: camel_case_types
-class getweather_cubit extends Cubit {
-  getweather_cubit(super.initialState);
-}
--- lib/cubits/getweather_cubit/getweather_states.dart
+++
@@ -1,7 +0,0 @@
-class weather_state {}
-
-class no_weather extends weather_state {}
-
-class weather_loaded extends weather_state {}
-
-class weather_faild extends weather_state {}
